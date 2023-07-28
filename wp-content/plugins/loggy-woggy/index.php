d106a6fcb8ff3e1bec732150fbec55fec2651617
dwdwork <dev@dwd.work>
2023-07-28 09:31:16 -0600

    WORKING

--- wp-content/plugins/loggy-woggy/index.php
+++ wp-content/plugins/loggy-woggy/index.php
@@ -41,10 +41,22 @@ include('./inc/header.php');
         
     <?php } else { ?>
         <?php include('./inc/app-parts/app-header.php'); ?>
-        <div id="app-contents" class="app-content col-12">
-            <h1>Better Bets Gamblin' Game</h1>
-            <h2>Welcome to the newest and greatest betting app <span class="username"><?php echo $_SESSION['username']; ?>!</span></h2>
-            <h3>Our app compiles the averages of the latest sports stats for each simulation. This keeps all the simulations you run up to date and 100% verified. Use it to gain an edge in your betting pools!</h3>
+        <div id="app-contents" class="app-content home-contents col-12">
+            <div class="row heading-content">
+                <div class="col-12 logo-banner">
+                    <img src="./assets/images/logo-banner.png">
+                </div>
+            </div>
+
+            <div class="row body-content">
+                <div class="col-12">
+                    <h2>Welcome <span class="username"><?php echo $_SESSION['username']; ?>!</span></h2>
+                    <h2>You just entered the realm of better betting. Take a look around and enjoy.</h2>
+                </div>
+                <div class="col-12">
+                <h3>Our app compiles the averages of the latest sports stats for each simulation. This keeps all the simulations you run up to date and 100% verified. Use it to gain an edge in your betting pools!</h3>
+                </div>
+            </div>
         </div>
         <?php include('./inc/app-parts/app-footer.php'); ?>
     <?php } ?>
