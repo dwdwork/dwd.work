d79718b7bb838f0cc9742fcf8d017bcbef704183
dwdwork <dev@dwd.work>
2023-07-31 17:30:12 -0600

    Updated fetches

--- loggy-woggy/inc/app-parts/stats.php
+++ loggy-woggy/inc/app-parts/stats.php
@@ -3,10 +3,15 @@
  * Holds user's profile info
  */
 
+// Get config 
+require_once('../config.php');
+
+// Get user data
+$user_data = getUserData();
 ?>
 <link href="./assets/css/stats.css" rel="stylesheet" type="text/css">
 <div id="stats-contents" class="app-part col-12">
-    <h1><?php echo $_SESSION['username']; ?> Stats</h1>
+    <h1><?php echo $user_data['username']; ?> Stats</h1>
     <div id="stats-container">
         <div class="row col team"></div>
     </div>
